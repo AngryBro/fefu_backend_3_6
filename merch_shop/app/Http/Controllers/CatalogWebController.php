67028f75555532b515ffe80eefd1327998ff18e2
AngryBro <67253990+AngryBro@users.noreply.github.com>
2022-06-23 19:37:47 +1000

    Lesson 9: Add search, sorting, filters to catalog (web & api)

--- merch_shop/app/Http/Controllers/CatalogWebController.php
+++ merch_shop/app/Http/Controllers/CatalogWebController.php
@@ -6,6 +6,9 @@ use App\Models\ProductCategory;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use App\Http\Filters\ProductFilter;
+use App\Http\Requests\CatalogFormRequest;
+use Exception;
 
 class CatalogWebController extends Controller
 {
@@ -14,31 +17,46 @@ class CatalogWebController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(?string $slug)
+    public function index(string $slug = null)
     {
         $query = ProductCategory::query()
             ->with('children','products');
-            if($slug===null) {
-                $query->where('parent_id');
-            }
-            else {
-                $query->where('slug',$slug);
-            }
+        if($slug===null) {
+            $query->where('parent_id');
+        }
+        else {
+            $query->where('slug',$slug);
+        }
 
-            $categories = $query->get();
+        $categories = $query->get();
+        $productQuery = ProductCategory::getTreeproductsBuilder($categories);
 
-            try{
-                $products = ProductCategory::getTreeProductsBuilder($categories)
-                    ->orderBy('id')
-                    ->paginate();
-            }catch(Exception $exception) {
-                abort(422, $exception->getMessage());
-            }
+        // try{
+        //     $productQuery = ProductCategory::getTreepro$productQueryBuilder($categories)
+        //         ->orderBy('id')
+        //         ->paginate();
+        // }catch(Exception $exception) {
+        //     abort(422, $exception->getMessage());
+        // }
 
-        return view('catalog',[
+        $filters = ProductFilter::build($productQuery, $requestData['filters'] ?? []);
+        ProductFilter::apply($productQuery, $requestData['filters'] ?? []);
+
+        if (isset($requestData['search_query'])) {
+            $productQuery->search($requestData['search_query']);
+        }
+
+        $sortMode = $requestData['sort_mode'] ?? null;
+        if ($sortMode === 'price_asc') {
+            $productQuery->orderBy('price');
+        } else if ($sortMode === 'price_desc') {
+            $productQuery->orderBy('price', 'desc');
+        }
+
+        return view('catalog', [
             'categories' => $categories,
-            'products' => $products
-    ]);
+            'products' => $productQuery->orderBy('products.id')->paginate(10),
+            'filters' => $filters
+        ]);
     }
-
 }
--- merch_shop/resources/views/catalog.blade.php
+++ merch_shop/resources/views/catalog.blade.php
@@ -6,8 +6,36 @@
         <title>Каталог</title>
     </head>
     <body>
-        Каталог
+        <form method="GET">
+            <div>
+                <label for="search_query">Поиск</label>
+                <input type="text" name="search_query" value="{{ request('search_query') }}">
+            </div>
+            <div>
+                <label for="sort_mode">Сортировка по</label>
+                <select name="sort_mode">
+                    <option value="price_asc" {{ request('sort_mode') === 'price_asc' ? 'selected' : '' }}>возрастанию цены</option>
+                    <option value="price_desc" {{ request('sort_mode') === 'price_desc' ? 'selected' : '' }}>убыванию цены</option>
+                </select>
+            </div>
+            @foreach($filters as $filter)
+                <div>
+                    <h4>{{ $filter->name }}</h4>
+                    @foreach($filter->options as $option)
+                        <div>
+                            <label>
+                                <input type="checkbox" value="{{ $option->value }}"
+                                name="filters[{{ $filter->key }}][]" {{ $option->isSelected ? 'checked' : ''}}>
+                                {{ $option->value }} ({{ $option->productCount }})
+                            </label>
+                        </div>
+                    @endforeach
+                </div>
+            @endforeach
+            <button>Применить</button>
+            </form>
         <div>
+            Каталог
             @include('catalogList', ['categories', $categories])
             @foreach ($products as $product)
             <article>
